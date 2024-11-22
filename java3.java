54829082d3635f5b4418cbfc49e914fba09b5318
isabelaisbrecht <isbrechtisa@gmail.com>
2024-11-22 09:18:41 -0300

    Create java3.java

---
+++ java3.java
@@ -0,0 +1,8 @@
+package Entidades; 
+public class Revista extends Produto{ 
+private String editora 
+public Revista(int codigo, String nome, String editora) { super (codigo, nome);
+ this. editora = editora;
+};
+
+}
