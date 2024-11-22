60bf199fbb2f9ab2f5056d84c169cf7344a2c9ab
isabelaisbrecht <isbrechtisa@gmail.com>
2024-11-22 09:18:10 -0300

    Create java2.java

---
+++ java2.java
@@ -0,0 +1,8 @@
+package Entidades; 
+public class Bebida extends Produto { 
+private String tipo; 
+}
+public Bebida (int codigo, String nome, String tipo) 
+{ super (codigo, nome) ;
+ this. tipo = tipo; 
+}
